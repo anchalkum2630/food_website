c1bd3d81ba9fec2607cbdcdb6fddcd6a06d527a2
Anchal kumari <anchalkum2630@gmail.com>
2025-04-12 17:08:25 +0530

    all done

--- Client/Components/FoodData.jsx
+++ Client/Components/FoodData.jsx
@@ -2,20 +2,20 @@ import React, { useState } from 'react';
 import { ImCross } from "react-icons/im";
 import { useViewContext } from './Context/Context_view';
 
-const item = [
-  {
-    "id": 1,
-    "name": "Thayir Semiya Recipe (Curd Semiya)",
-    "image_url": "https://www.archanaskitchen.com/images/archanaskitchen/1-Author/Raksha_Kamat/Thayir_Curd_Semiya_recipe_Yogurt_Vermicelli_South_indian_Lunch_recipe-4.jpg",
-    "description": "Thayir Semiya or Curd Vermicelli is a quick dish which you can make for lunch. If you are bored of eating curd rice every day, you can always make this Thayir Semiya or curd Semiya for a change. In South India, people consume curd every day.",
-    "cuisine": "Indian",
-    "course": "Lunch",
-    "diet": "Vegetarian",
-    "prep_time": "Total in 35 M",
-    "ingredients": "1/2 cup Semiya (Vermicelli), roasted\n1 cup Curd (Dahi / Yogurt)\nFor tempering:\n1 teaspoon Mustard seeds (Rai/ Kadugu)\n1/2 teaspoon White Urad Dal (Split)\nPinch Asafoetida (hing)\n1 sprig Curry leaves",
-    "instructions": "To begin making the Thayir Semiya recipe, firstly boil the vermicelli in 2 cups water. Drain it and spread on a colander. Pass cold water through it. This ensures that the vermicelli does not become sticky. Keep it aside and let it cool down. Heat a kadai/wok and add oil. Add the peanuts and roast till it turns brownish. Remove and keep it aside. In the same kadai, add more oil if needed and add the mustard seeds and after it splutters add the other ingredients meant for tempering. Add the boiled vermicelli and mix everything. Add salt to taste and mix. Finally just before serving, add curd and mix well. Serve Thayir Semiya on its own for a light and healthy lunch or dinner."
-  }
-];
+// const item = [
+//   {
+//     "id": 1,
+//     "name": "Thayir Semiya Recipe (Curd Semiya)",
+//     "image_url": "https://www.archanaskitchen.com/images/archanaskitchen/1-Author/Raksha_Kamat/Thayir_Curd_Semiya_recipe_Yogurt_Vermicelli_South_indian_Lunch_recipe-4.jpg",
+//     "description": "Thayir Semiya or Curd Vermicelli is a quick dish which you can make for lunch. If you are bored of eating curd rice every day, you can always make this Thayir Semiya or curd Semiya for a change. In South India, people consume curd every day.",
+//     "cuisine": "Indian",
+//     "course": "Lunch",
+//     "diet": "Vegetarian",
+//     "prep_time": "Total in 35 M",
+//     "ingredients": "1/2 cup Semiya (Vermicelli), roasted\n1 cup Curd (Dahi / Yogurt)\nFor tempering:\n1 teaspoon Mustard seeds (Rai/ Kadugu)\n1/2 teaspoon White Urad Dal (Split)\nPinch Asafoetida (hing)\n1 sprig Curry leaves",
+//     "instructions": "To begin making the Thayir Semiya recipe, firstly boil the vermicelli in 2 cups water. Drain it and spread on a colander. Pass cold water through it. This ensures that the vermicelli does not become sticky. Keep it aside and let it cool down. Heat a kadai/wok and add oil. Add the peanuts and roast till it turns brownish. Remove and keep it aside. In the same kadai, add more oil if needed and add the mustard seeds and after it splutters add the other ingredients meant for tempering. Add the boiled vermicelli and mix everything. Add salt to taste and mix. Finally just before serving, add curd and mix well. Serve Thayir Semiya on its own for a light and healthy lunch or dinner."
+//   }
+// ];
 
 const FoodData = () => {
   const {close,foodDetail,onClose}=useViewContext();
